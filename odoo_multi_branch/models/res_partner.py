bca0beba703a2168b92e42b4ad076542ba5525d8
uamsheikh <uamsheikh95@gmail.com>
2020-01-09 14:01:19 +0300

    Multbr debugging

--- odoo_multi_branch/models/res_partner.py
+++ odoo_multi_branch/models/res_partner.py
@@ -15,25 +15,25 @@ class ResPartner(models.Model):
 		copy=False,
 		default=lambda self: self.env.user.company_branch_id.id)
 
-	@api.model
-	def _search(self, args, offset=0, limit=None, order=None, count=False, access_rights_uid=None):
-		admin_user = self.env.user.has_group('base.group_erp_manager')
-		if not admin_user and not self._context.get("from_multi_branch", False):
-			args = ['|',('company_branch_id','=',False), ('company_branch_id', '=', self.env.user.company_branch_id.id)] + list(args)
-		return super(ResPartner, self)._search(args, offset, limit, order, count, access_rights_uid)
-
-
-	# @api.multi
-	def read(self, fields=None, load='_classic_read'):
-		read_partner = super(ResPartner, self).read(fields, load=load)
-		admin_user = self.env.user.has_group('base.group_erp_manager')
-		if admin_user or self._context.get("from_multi_branch", False):
-			return read_partner
-		for partner in read_partner:
-			if partner.get("company_branch_id") and self.env.user.company_branch_id.id != partner["company_branch_id"]:
-				if isinstance(partner["company_branch_id"] , tuple) and self.env.user.company_branch_id.id != partner["company_branch_id"][0]:
-					raise ValidationError("You are not allowed to access")
-		return read_partner
+	# @api.model
+	# def _search(self, args, offset=0, limit=None, order=None, count=False, access_rights_uid=None):
+	# 	admin_user = self.env.user.has_group('base.group_erp_manager')
+	# 	if not admin_user and not self._context.get("from_multi_branch", False):
+	# 		args = ['|',('company_branch_id','=',False), ('company_branch_id', '=', self.env.user.company_branch_id.id)] + list(args)
+	# 	return super(ResPartner, self)._search(args, offset, limit, order, count, access_rights_uid)
+#
+#
+# 	# @api.multi
+# 	def read(self, fields=None, load='_classic_read'):
+# 		read_partner = super(ResPartner, self).read(fields, load=load)
+# 		admin_user = self.env.user.has_group('base.group_erp_manager')
+# 		if admin_user or self._context.get("from_multi_branch", False):
+# 			return read_partner
+# 		for partner in read_partner:
+# 			if partner.get("company_branch_id") and self.env.user.company_branch_id.id != partner["company_branch_id"]:
+# 				if isinstance(partner["company_branch_id"] , tuple) and self.env.user.company_branch_id.id != partner["company_branch_id"][0]:
+# 					raise ValidationError("You are not allowed to access")
+# 		return read_partner
 class ResPartnerCategory(models.Model):
 	_inherit = 'res.partner.category'
 
