5861d739a97e4099201a68057c693013869ff13c
uamsheikh <uamsheikh95@gmail.com>
2020-02-11 17:37:13 +0300

    statement send

--- send_customer_statement_by_email/wizard/account_report_partner_ledger.py
+++ send_customer_statement_by_email/wizard/account_report_partner_ledger.py
@@ -188,8 +188,7 @@ class AccountPartnerLedger(models.TransientModel):
         partners = obj_partner.browse(self.env.context.get('active_id'))
         partners = sorted(partners, key=lambda x: (x.ref, x.name))
 
-        print('--------ppppppppppppppppp----------------------')
-        print(partners)
+
 
         mail_obj = self.env['mail.mail']
 
@@ -205,9 +204,12 @@ class AccountPartnerLedger(models.TransientModel):
         body_html = ""
 
         for o in partners:
-            current_balance = self._sum_partner(data, o, 'debit - credit')
 
-            body_html = body_html + """
+            # print('///////////////////////////////////////////////////////////////////////////////////////////////////////')
+            # print(self._lines(data, o))
+            # print('///////////////////////////////////////////////////////////////////////////////////////////////////////')
+
+            search_criteria = """
             <div style='width: 100%'>
                 <div style='width: 33%;float: left'>
                     <strong>Company: </strong><br/>""" + str(company_id) + """
@@ -227,53 +229,126 @@ class AccountPartnerLedger(models.TransientModel):
             </div>
 
             <br/>
-
+            """
+            partner_ref = o.ref if  o.ref else ''
+            table = """
+            <h3>""" + partner_ref + """- """ + o.name + """</h3>
             <table style="font-family: arial, sans-serif; border-collapse: collapse; width: 100%;border-top: 1px solid#ddd;border-bottom: 1px solid#ddd;">
               <thead>
                   <tr>
                 	<th style="border: 1 px solid #dddddd; text-align: left; padding: 8px;">Date</th>
                 	<th style="border: 1 px solid #dddddd; text-align: left; padding: 8px;">JRNL</th>
                 	<th style="border: 1 px solid #dddddd; text-align: left; padding: 8px;">Account</th>
-                	<th style="border: 1 px solid #dddddd; text-align: left; padding: 8px;">Debit</th>
-                	<th style="border: 1 px solid #dddddd; text-align: left; padding: 8px;">Credit</th>
-                	<th style="border: 1 px solid #dddddd; text-align: left; padding: 8px;">Balance</th>
-                  </tr>
-              </thead>
+                    <th style="border: 1 px solid #dddddd; text-align: left; padding: 8px;">Ref</th>
+                	<th style="border: 1 px solid #dddddd; text-align: right; padding: 8px;">Debit</th>
+                	<th style="border: 1 px solid #dddddd; text-align: right; padding: 8px;">Credit</th>
+                	<th style="border: 1 px solid #dddddd; text-align: right; padding: 8px;">Balance</th>
+                    </tr></thead>
+                    """
 
-              <tbody>
-                <tr>
-    				<td colspan='7'>
-						<strong>""" + o.ref if  o.ref else '' + """</strong>""" + """- <strong>""" + o.name + """</strong>
-    				</td>
-        		</tr>"""
+                  # table =  table + "</tr></thead>"
+
+            table = table + "<tbody>"
+
 
             if date_from:
                 balance_forward = self._sum_open_balance(data, o, 'debit - credit')
-                body_html = body_html + """<tr>
+                table = table + """<tr style="border-bottom: 1px solid #dddd;border-top: 1px solid #ddd;">
                     <td colspan="4">
 						<strong>-Balance Forward</strong>
 					</td>
 
                     <td>
-                        <strong> """ + str(self._sum_open_balance(data, o, 'debit')) + """</strong>
+                        <!--<strong> """ + str(self._sum_open_balance(data, o, 'debit')) + """</strong>-->
                     </td>
 
                     <td>
-                        <strong> """ + str(self._sum_open_balance(data, o, 'credit')) + """</strong>
+                        <!--<strong> """ + str(self._sum_open_balance(data, o, 'credit')) + """</strong>-->
                     </td>
 
                     <td>
-                        <p><strong>""" + str(balance_forward) + """</strong></p>
+                        <p><strong>""" + str('{:,.2f}'.format(balance_forward)) + """</strong></p>
                     </td>
                 </tr>"""
+
+            for line in self._lines(data, o):
+                table = table + """
+                    <tr style="border-bottom: 1px solid #dddd;border-top: 1px solid #ddd;">
+                        <td>
+        						<span>""" + str(line['date']) + """</span>
+        				</td>
+        				<td>
+                                <span>""" + line['code'] + """</span>
+        				</td>
+        				<td>
+                                <span>""" + line['a_code'] + """</span>
+        				</td>
+        				<td>
+        						<span>""" + line['displayed_name'] + """</span>
+        				</td>
+        				<td style="text-align: right">
+        						<span>""" + str('{:,.2f}'.format(line['debit'])) + """</span>
+        				</td>
+        				<td style="text-align: right">
+        						<span>""" + str('{:,.2f}'.format(line['credit'])) + """</span>
+        				</td>
+        				<td style="text-align: right">
+        						<span>""" + str('{:,.2f}'.format(line['progress']) ) + """</span>
+        				</td>
+                        </tr>
+                        """
+                            # if data['form']['amount_currency']:
+                			# 	table = table + """<td></span>
+                			# 			<span>line['amount_currency'] line['currency_code']</span>
+                            #             <span>""" + line['amount_currency'] + """</span><span>""" + line['currency_code'] + """</span>
+                			# 	</td>"""
+
+            current_balance = self._sum_partner(data, o, 'debit - credit')
+
+            table = table + """<tr style="border-bottom: 1px solid #dddd;border-top: 1px solid #ddd;">
+                <td colspan="4">
+					<strong>-Balance</strong>
+				</td>
+
+                <td>
+                    <!--<strong> """ + str(self._sum_open_balance(data, o, 'debit')) + """</strong>-->
+                </td>
+
+                <td>
+                    <!--<strong> """ + str(self._sum_open_balance(data, o, 'credit')) + """</strong>-->
+                </td>
+
+                <td>
+                    <p><strong>""" + str('{:,.2f}'.format(current_balance)) + """</strong></p>
+                </td>
+            </tr>
+            """
+
+            if date_from:
+                amount_due = current_balance+balance_forward
+                table = table + """
+                <tr style="border-bottom: 1px solid #dddd;border-top: 1px solid #ddd;">
+                		<td colspan="6">
+                						<strong>Total Amount Due</strong>
+                		</td>
+
+                		<td class="text-right">
+                               <strong>""" + str('{:,.2f}'.format(amount_due)) +"""</strong>
+                		</td>
+                </tr>
+                """
+
                 #str(balance_forward)
+            table = table + "</tbody></table>"
 
 
 
 
-        body_html = body_html + "</tbody></table>"
+        body_html = search_criteria + table
+
 
         mail = mail_obj.create({
+            'headers': '[' + company_id + '] Customer Statement',
             'body_html': body_html,
             'email_to': ('uamsheikh95@gmail.com')
         })
