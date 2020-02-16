acdcab9457e71af54754b939474a6e560ab31582
uamsheikh <uamsheikh95@gmail.com>
2020-02-16 12:16:34 +0300

    Search patient by it's email and name feature

--- oehealth/oeh_medical/models/oeh_medical.py
+++ oehealth/oeh_medical/models/oeh_medical.py
@@ -221,6 +221,28 @@ class OeHealthPatient(models.Model):
         ('code_oeh_patient_userid_uniq', 'unique (oeh_patient_user_id)', "Selected 'Responsible' user is already assigned to another patient !")
     ]
 
+    @api.multi
+    def name_get(self):
+        result = []
+        for record in self:
+            if record.mobile:
+                name = str(record.mobile) + ' - ' + record.name
+            else:
+                name = record.name
+            result.append((record.id, name))
+        return result
+
+    @api.model
+    def name_search(self, name, args=None, operator='ilike', limit=100):
+        args = args or []
+        recs = self.browse()
+        if name:
+            recs = self.search((args + ['|', ('mobile', 'ilike', name), ('name', 'ilike', name)]),
+                               limit=limit)
+        if not recs:
+            recs = self.search([('name', operator, name)] + args, limit=limit)
+        return recs.name_get()
+
     @api.model
     def create(self, vals):
         sequence = self.env['ir.sequence'].next_by_code('oeh.medical.patient')
