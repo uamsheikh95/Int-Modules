19405bab08536c9bd45a2aaf7f33dd3cbdc6266b
uamsheikh <uamsheikh95@gmail.com>
2020-09-23 12:07:35 +0300

    Bring some changes to the module

--- fleet_addons/models/fleet.py
+++ fleet_addons/models/fleet.py
@@ -6,12 +6,14 @@ class FleetVehicleLogFuel(models.Model):
     _inherit = 'fleet.vehicle.log.fuel'
 
     fuel_type = fields.Selection([('diesel', 'Diesel'), ('petrol', 'Petrol')])
+    company_branch_id = fields.Many2one('res.company.branch', string="Branch", copy=False, default=lambda self: self.env.user.company_branch_id.id)
 
 class FleetVehicleLogServices(models.Model):
     _inherit = 'fleet.vehicle.log.services'
 
     fuel_type = fields.Selection([('diesel', 'Diesel'), ('petrol', 'Petrol')])
     item_ids = fields.One2many('fleet_addons.services_items', 'fleet_service_id')
+    company_branch_id = fields.Many2one('res.company.branch', string="Branch", copy=False, default=lambda self: self.env.user.company_branch_id.id)
 
 class FleetServicesItems(models.Model):
     _name = 'fleet_addons.services_items'
