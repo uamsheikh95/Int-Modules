43a6896a35af99b6aec3d0b5e7619e533ff4b27f
uamsheikh <uamsheikh95@gmail.com>
2020-09-28 09:57:03 +0300

    Costruction Management V13.0

---
+++ construction_management_app/models/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+from . import stock_picking
+from . import task
+from . import project
+from . import note
+from . import purchase
+from . import product
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
---
+++ construction_management_app/wizard/__init__.py~
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+
+import purchase_order
+import project_user_subtask
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
