22c2945c91acc5aa86fd17f13dac9e580a294f79
uamsheikh <uamsheikh95@gmail.com>
2020-10-04 10:47:11 +0300

    material selection option

--- construction_management_app/models/product.py
+++ construction_management_app/models/product.py
@@ -4,11 +4,12 @@ from odoo import models, fields
 
 class Product(models.Model):
     _inherit = 'product.product'
-    
+
     boq_type = fields.Selection([
         ('eqp_machine', 'Machinery / Equipment'),
+        ('material', 'Material'),
         ('worker_resource', 'Worker / Resource'),
         ('work_cost_package', 'Work Cost Package'),
-        ('subcontract', 'Subcontract')], 
-        'BOQ Type', 
+        ('subcontract', 'Subcontract')],
+        'BOQ Type',
     )
